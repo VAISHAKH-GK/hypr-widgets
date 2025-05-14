9288b8939c0f450dc05a63d35404657468dea52c
Vaishakh GK <vaishakhgk2006@gmail.com>
2025-05-14 21:09:09 +0530

    Change scss path & Remove Bar
      * Change .scss files path
    
      * Remove bar in app.ts
    
      * Add background color to clock widget

--- app.ts
+++ app.ts
@@ -1,14 +1,12 @@
 import { App } from "astal/gtk3";
 import style from "./style.scss";
 import DesktopClock from "./widget/DesktopClock";
-import Bar from "./widget/Bar";
 import ChargingAlert from "./widget/ChargingAlert";
 
 App.start({
   css: style,
   main() {
     App.get_monitors().forEach((gdkmonitor) => {
-      Bar(gdkmonitor);
       DesktopClock(gdkmonitor);
       ChargingAlert(gdkmonitor);
     });
